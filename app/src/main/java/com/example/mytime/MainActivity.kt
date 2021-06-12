c8d24cfa07e946be1c2e5264b6b2d6cec02378ce
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-06-12 18:54:44 -0500

    Gif begin

--- app/src/main/java/com/example/mytime/MainActivity.kt
+++ app/src/main/java/com/example/mytime/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.mytime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.web_view)
+        var ws = webView.getSettings()
+        ws.setJavaScriptEnabled(true)
+
     }
 
 }
