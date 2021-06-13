21bbe088fb9f7d4327b4928b461ad68ef86e5268
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-06-12 21:31:30 -0500

    design for rests

--- app/src/main/java/com/example/mytime/MainActivity.kt
+++ app/src/main/java/com/example/mytime/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mytime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
+    //private var background: ConstraintLayout = findViewById(R.id.constraintLayout)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +18,9 @@ class MainActivity : AppCompatActivity() {
         webView = findViewById(R.id.web_view)
         var ws = webView.getSettings()
         ws.setJavaScriptEnabled(true)
-
+        var file = "file:android_asset/pequeño.gif"
+        ws.setLoadWithOverviewMode(true);
+        ws.setUseWideViewPort(true);
+        webView.loadUrl(file)
     }
-
 }
