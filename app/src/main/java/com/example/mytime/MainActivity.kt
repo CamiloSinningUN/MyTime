f7615525c7d16bb8ba5f6c1753eef6711d4efab0
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-06-14 22:32:46 -0500

    timer Begin

--- app/src/main/java/com/example/mytime/MainActivity.kt
+++ app/src/main/java/com/example/mytime/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mytime
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,12 +9,26 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.progressindicator.LinearProgressIndicator
 
+enum class State {
+    Studying, Resting
+}
+
+enum class TimerState{
+    Stopped,Paused,Running
+}
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
     private lateinit var fab: FloatingActionButton
     private lateinit var layout: ConstraintLayout
     private lateinit var cardViewLayout: ConstraintLayout
     private lateinit var linearIndicator: LinearProgressIndicator
+
+    private var state: State = State.Resting
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds = 0L
+    private var timerState = TimerState.Stopped
+    private var secondsRemaining = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +38,13 @@ class MainActivity : AppCompatActivity() {
         restTime()
 
         fab.setOnClickListener {
-            studyTime()
+            if(state == State.Resting){
+                studyTime()
+                state = State.Studying
+            }else if(state == State.Studying){
+                restTime()
+                state = State.Resting
+            }
         }
     }
 
@@ -37,18 +58,32 @@ class MainActivity : AppCompatActivity() {
 
     fun studyTime(){
         studyTheme()
+        startTimer()
+        timerState = TimerState.Running
     }
 
     fun studyTheme(){
         layout.setBackgroundColor(Color.parseColor("#12664f"))
         cardViewLayout.setBackgroundColor(Color.parseColor("#307473"))
-        fab.setBackgroundColor(Color.parseColor("#7a82ab"))
+        fab.setImageResource(R.drawable.ic_baseline_stop_24)
         gifStudy(webView)
     }
     fun restTime(){
         restTheme()
+        timer.cancel()
+        timerState = TimerState.Stopped
+        onTimerFinished()
     }
+
+    override fun onResume(){
+        super.onResume()
+        initTimer()
+    }
+
     fun restTheme(){
+        layout.setBackgroundColor(Color.parseColor("#13293d"))
+        cardViewLayout.setBackgroundColor(Color.parseColor("#2a628f"))
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
         gifRest(webView)
     }
 
@@ -56,8 +91,8 @@ class MainActivity : AppCompatActivity() {
         var ws = webView.getSettings()
         ws.setJavaScriptEnabled(true)
         var file = "file:android_asset/pequeño.gif"
-        ws.setLoadWithOverviewMode(true);
-        ws.setUseWideViewPort(true);
+        ws.loadWithOverviewMode = true;
+        ws.useWideViewPort = true;
         webView.loadUrl(file)
     }
 
@@ -65,8 +100,8 @@ class MainActivity : AppCompatActivity() {
         var ws = webView.getSettings()
         ws.setJavaScriptEnabled(true)
         var file = "file:android_asset/study2.gif"
-        ws.setLoadWithOverviewMode(true);
-        ws.setUseWideViewPort(true);
+        ws.loadWithOverviewMode = true;
+        ws.useWideViewPort = true;
         webView.loadUrl(file)
     }
 
