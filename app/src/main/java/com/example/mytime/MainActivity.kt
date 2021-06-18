54dbf10e24c2bf7361203c80115a3191409c0bb4
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-06-17 23:03:44 -0500

    new desing and status

--- app/src/main/java/com/example/mytime/MainActivity.kt
+++ app/src/main/java/com/example/mytime/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.mytime
 
-import android.content.res.ColorStateList
+import android.app.AlertDialog
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
+import android.view.Window
+import android.view.WindowManager
 import android.webkit.WebView
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -22,7 +25,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fab: FloatingActionButton
     private lateinit var layout: ConstraintLayout
     private lateinit var cardViewLayout: ConstraintLayout
+    private lateinit var cardViewLayoutStatus: ConstraintLayout
     private lateinit var linearIndicator: LinearProgressIndicator
+    private lateinit var actionBar: ActionBar
+    private lateinit var textStatus: TextView
 
     private lateinit var restArray: ArrayList<LinearProgressIndicator>
 
@@ -39,7 +45,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         initialize()
         inactiveTime()
 
@@ -47,9 +53,26 @@ class MainActivity : AppCompatActivity() {
             if(state == State.Inactive){
                 studyTime()
             }else if(state == State.Resting || state == State.Studying){
-                inactiveTime()
+                areYouSureToStop()
+
             }
         }
+    }
+
+    private fun areYouSureToStop() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Detener")
+        builder.setMessage("¿Está seguro que desea dejar de Estudiar?")
+
+        builder.setPositiveButton("Sí") { dialog, which ->
+            inactiveTime()
+        }
+
+        builder.setNegativeButton("No") { dialog, which ->
+        }
+        builder.show()
+
+
     }
 
     private fun inactiveTime() {
@@ -103,39 +126,57 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun inactiveTheme() {
+        window.statusBarColor = Color.parseColor("#1C1519")
         layout.setBackgroundColor(Color.parseColor("#1C1519"))
         cardViewLayout.setBackgroundColor(Color.parseColor("#454545"))
         fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
-        gifRest(webView)
+        textStatus.setTextColor(Color.parseColor("#CED0CE"))
+        textStatus.setText("Inactivo")
+        actionBar.setBackgroundDrawable(ColorDrawable(Color.parseColor("#454545")))
+        cardViewLayoutStatus.setBackgroundColor(Color.parseColor("#454545"))
+        gifInactive(webView)
     }
 
     fun studyTheme(){
+        window.statusBarColor = Color.parseColor("#12664f")
         layout.setBackgroundColor(Color.parseColor("#12664f"))
         cardViewLayout.setBackgroundColor(Color.parseColor("#307473"))
+        textStatus.setTextColor(Color.parseColor("#c6d4ff"))
+        textStatus.setText("Estudiando")
+        cardViewLayoutStatus.setBackgroundColor(Color.parseColor("#307473"))
         fab.setImageResource(R.drawable.ic_baseline_stop_24)
+        actionBar.setBackgroundDrawable(ColorDrawable(Color.parseColor("#307473")))
         gifStudy(webView)
     }
 
     fun restTheme(){
+        window.statusBarColor = Color.parseColor("#13293d")
         layout.setBackgroundColor(Color.parseColor("#13293d"))
         cardViewLayout.setBackgroundColor(Color.parseColor("#2a628f"))
+        cardViewLayoutStatus.setBackgroundColor(Color.parseColor("#2a628f"))
         fab.setImageResource(R.drawable.ic_baseline_stop_24)
+        textStatus.setTextColor(Color.parseColor("#4DFFF3"))
+        textStatus.setText("Descansando")
+        actionBar.setBackgroundDrawable(ColorDrawable(Color.parseColor("#2a628f")))
         gifRest(webView)
     }
 
     fun initialize(){
         webView = findViewById(R.id.web_view)
         fab = findViewById(R.id.floatingActionButton2)
-        layout = findViewById(R.id.constraintLayout)
+        layout = findViewById(R.id.constraint_layout)
         cardViewLayout = findViewById(R.id.cardViewLayout)
         linearIndicator = findViewById(R.id.linearProgressIndicator2)
+        actionBar = supportActionBar!!
+        actionBar.hide()
+        textStatus = findViewById(R.id.textView)
+        cardViewLayoutStatus = findViewById(R.id.status)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         restArray = ArrayList<LinearProgressIndicator>(4)
         restArray.add(findViewById(R.id.progressBar5))
         restArray.add(findViewById(R.id.progressBar6))
         restArray.add(findViewById(R.id.progressBar7))
         restArray.add(findViewById(R.id.progressBar8))
-
-
     }
 
     fun startTimer(){
@@ -179,6 +220,15 @@ class MainActivity : AppCompatActivity() {
         var ws = webView.settings
         ws.javaScriptEnabled = true
         var file = "file:android_asset/study2.gif"
+        ws.loadWithOverviewMode = true;
+        ws.useWideViewPort = true;
+        webView.loadUrl(file)
+    }
+
+    fun gifInactive(webView: WebView){
+        var ws = webView.settings
+        ws.javaScriptEnabled = true
+        var file = "file:android_asset/inactive.gif"
         ws.loadWithOverviewMode = true;
         ws.useWideViewPort = true;
         webView.loadUrl(file)
